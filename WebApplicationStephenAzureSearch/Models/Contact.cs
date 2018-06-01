e84895a9bfede06d1dc9a4c9a469b1ef8c32f716
StephenGregan <grego@live.ie>
2018-06-01 09:40:12 +0100

    new update

--- WebApplicationStephenAzureSearch/Models/Contact.cs
+++ WebApplicationStephenAzureSearch/Models/Contact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.Search.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,13 @@ namespace WebApplicationStephenAzureSearch.Models
 {
     public class Contact
     {
+        public FacetResults Facets { get; set; }
+        public IList<SearchResult> Results { get; set; }
+        public int? Count { get; set; }
+    }
+
+    public class ContactLookup
+    {
+        public Document Result { get; set; }
     }
 }
